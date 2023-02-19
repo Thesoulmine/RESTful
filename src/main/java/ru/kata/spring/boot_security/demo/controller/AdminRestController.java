8c0bf1231d739df68f09bf351975a5ab23b6c670
Thesoulmine <mohk223@gmail.com>
2023-02-19 15:39:32 +0300

    RESTful

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -26,6 +27,7 @@ public class AdminRestController {
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> showAllUsers() {
+        List<User> users = userService.getUsersList();
         return ResponseEntity.ok().body(userService.getUsersList());
     }
 
@@ -35,26 +37,31 @@ public class AdminRestController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Void> addNewUser(@RequestBody User user) {
+        public ResponseEntity<User> addNewUser(@RequestBody User user) {
         userService.saveUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/users")
-    public ResponseEntity<Void> updateUser(@RequestBody User user) {
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
         userService.updateUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
         userService.deleteUserById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/users/current")
     public ResponseEntity<User> getCurrentUser(Principal principal) {
         return new ResponseEntity<>(userService.getUserByUsername(
                 principal.getName()), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/roles")
+    public ResponseEntity<List<Role>> getRoles() {
+        return new ResponseEntity<>(roleService.getRolesList(), HttpStatus.OK);
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -17,6 +17,14 @@ public class Role implements GrantedAuthority {
 
     private String name;
 
+    public Role() {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
